156776b4e90a3c3e842a9f7fc004fd6eee15f0d2
Natedorleus <ndorleus@gmail.com>
2025-09-18 08:54:21 -0400

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,11 @@
+#Nathan
+#09 Sept 2025
+#P1LAB1
+# This program gets the input from the user, promting first name and last name
+
+
+Firstname=input("Enter your name: ")
+
+Lastname=input("Enter your last name: ")
+
+print("Hello," ,Firstname ,Lastname+ "!" " Welcome to CTI-110")
